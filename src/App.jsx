c80e34bd87a248e6d1b877a328ec2743c5947e7a
AlexSvet117 <alex.svet117@gmail.com>
2025-01-27 12:11:31 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import Logo from './components/Logo';
+import { useState } from "react";
 
 function App() {
+
+  // Primitive data types
+  const [firstName, setFirstName] = useState('John Doe')
+  const [count, setCount] = useState(1)
+  const [isCoder, setIsCoder] = useState(true)
+
+  // Arrays
+  useState()
+
+
+
   return (
+    
     <>
-      <Logo />
+    <div className="container text-center my-5">
+      <h1>Primitive</h1>
+      <p>{firstName}</p>
+      <p>{count}</p>
+      <p>{isCoder ? "Coder" : "Not Coder"}</p>
+      <button className="btn btn-primary"
+      onClick={() => {
+        setFirstName("John Doe New")
+        setCount(count+1)
+        setIsCoder(!isCoder)
+      }}>Update</button>
+    </div>
+     
     </>
   );
 }
