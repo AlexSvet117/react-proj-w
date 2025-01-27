113871366ce139ba28019633e0a8a9ec01a800c5
AlexSvet117 <alex.svet117@gmail.com>
2025-01-27 15:33:51 +0000

    Final commits

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,49 @@ export default function() {
         {id: 1, userName: "JohnD", imageUrl: "https://picsum.photos/200?random=3", likeCount: 19, caption: "Yo"}
     ])
 
+    const [formData, setFormData] = useState({
+        userName: "",
+        imageLink: "",
+        caption: ""
+    })
+
+    const createPost = () => {
+        const newUserName = formData.userName
+        const newImageLink = formData.imageLink
+        const newCaption = formData.caption
+
+    if (newUserName && newImageLink && newCaption) {
+        setPosts([...posts,{
+            id: posts.length + 1,
+            userName: newUserName,
+            imageUrl: newImageLink,
+            likeCount: 0,
+            caption: newCaption
+        }])
+
+    setFormData ({
+        userName: "",
+        imageLink: "",
+        caption: ""
+    })
+    }}
+
 
     return (
         <>
         <div className="container text-center mt-5">
             <h1 className="mb-3">My Feed</h1>
+
+            <div className="container col-md-6 mb-5">
+                <input className="form-control mb-2" type="text" placeholder="User Name" 
+                value={formData.username} onChange={e => setFormData({...formData, userName: e.target.value})}/>
+                <input className="form-control mb-2" type="text" placeholder="Image Link" 
+                value={formData.imageLink} onChange={e => setFormData({...formData, imageLink: e.target.value})}/>
+                <input className="form-control mb-3" type="text" placeholder="Caption" 
+                value={formData.caption} onChange={e => setFormData({...formData, caption: e.target.value})}/>
+                <button className="btn btn-primary" onClick={createPost}>Create Post</button>
+
+            </div>
 
             <div className="container">
                 {posts && (
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,13 @@
+import { FaRegHeart } from "react-icons/fa";
+
 export default function Post({post}) {
     return (
         <div className="card container col-md-4 p-0 mb-4">
-            <strong className="py-2 text-start ps-3">{post.userName}</strong>
+            <strong className="py-2 text-start ps-3">@{post.userName}</strong>
             <div className="">
                 <img src={post.imageUrl} className="w-100" alt="image" />
             </div>
-            <p className="text-start ps-3 pt-3">{post.likeCount} likes</p>
+            <p className="text-start ps-3 pt-3"><FaRegHeart/> {post.likeCount} likes</p>
             <p className="text-start ps-3"><strong className="pe-1">{post.userName}</strong>{post.caption}</p>
         </div>
     )
